26f824922bceec6dbf2ef0eea2d1981cce6afc77
Codica Studio <CodicaStudioDev@gmail.com>
2020-08-17 21:38:59 -0500

    Update

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -451,7 +451,7 @@ Fully Tested & Complete   - #4c956c
         </div>  
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#e71d36"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Spatie Tags</text></svg>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#ff9f1c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Spatie Tags</text></svg>
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
